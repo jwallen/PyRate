b2fb3a58000d52f64d51ad6fe306940244665d16
Josh Allen <jwallen@mit.edu>
2012-01-06 15:34:33 -0500

    Added many more unit conversion functions to pyrate.units module.
    The new unit conversion functions are: energy (and enthalpy and free
    energy), heat capacity (and entropy), frequency, mass, moment of
    inertia, and rate coefficient. Note that energies, heat capacities, and
    masses will automatically be converted between extensive and intensive
    (molar) values. Rate coefficients will also be converted between molar
    and molecular units as long as the "mol" or "molecule" is specified
    explicitly.

--- pyrate/units.py
+++ pyrate/units.py
@@ -121,3 +121,249 @@ def convertPressure(quantity, units):
         raise ValueError('Invalid output units "{0}" for pressure.'.format(units))
         
     return quantity.rescale(units)
+
+################################################################################
+
+# Allowed energy units
+ENERGY_DIMENSIONS = [pq.J.simplified.dimensionality]
+MOLAR_ENERGY_DIMENSIONS = [(pq.J / pq.mol).simplified.dimensionality]
+
+def convertEnergy(quantity, units, unittype='energy'):
+    """
+    Convert a given `quantity` -- a :class:`Quantity` object with units of 
+    energy -- to the given `units` of energy. A :class:`ValueError` is raised
+    if this conversion is not successful. This function can handle conversion
+    between intensive (molar) and extensive energies by multiplying or dividing
+    by an Avogadro number as appropriate.
+    """
+    if isinstance(quantity, tuple):
+        quantity = pq.Quantity(quantity[0], quantity[1])
+    elif not isinstance(quantity, pq.Quantity):
+        raise ValueError('Invalid value "{0}" for quantity; must be a Quantity object with units of {1}.'.format(quantity, unittype))
+    
+    if isinstance(units, str):
+        units = pq.Quantity(1.0, units)
+    inputDimensionality = quantity.units.dimensionality.simplified
+    outputDimensionality = units.dimensionality.simplified
+    
+    if inputDimensionality not in ENERGY_DIMENSIONS and inputDimensionality not in MOLAR_ENERGY_DIMENSIONS:
+        raise ValueError('Invalid input units "{0}" for {1}.'.format(quantity.units, unittype))
+    if outputDimensionality not in ENERGY_DIMENSIONS and outputDimensionality not in MOLAR_ENERGY_DIMENSIONS:
+        raise ValueError('Invalid output units "{0}" for {1}.'.format(units, unittype))
+    
+    if inputDimensionality in ENERGY_DIMENSIONS and outputDimensionality in MOLAR_ENERGY_DIMENSIONS:
+        quantity = quantity * constants.Na / pq.mol
+    elif inputDimensionality in MOLAR_ENERGY_DIMENSIONS and outputDimensionality in ENERGY_DIMENSIONS:
+        quantity = quantity / (constants.Na / pq.mol)
+    
+    return quantity.rescale(units)
+
+def convertEnthalpy(quantity, units):
+    """
+    Convert a given `quantity` -- a :class:`Quantity` object with units of 
+    enthalpy -- to the given `units` of enthalpy. A :class:`ValueError` is raised
+    if this conversion is not successful. This function can handle conversion
+    between intensive (molar) and extensive enthalpies by multiplying or
+    dividing by an Avogadro number as appropriate.
+    """
+    return convertEnergy(quantity, units, 'enthalpy')
+
+def convertFreeEnergy(quantity, units):
+    """
+    Convert a given `quantity` -- a :class:`Quantity` object with units of 
+    free energy -- to the given `units` of free energy. A :class:`ValueError` 
+    is raised if this conversion is not successful. This function can handle 
+    conversion between intensive (molar) and extensive free energies by
+    multiplying or dividing by an Avogadro number as appropriate.
+    """
+    return convertEnergy(quantity, units, 'free energy')
+
+################################################################################
+
+# Allowed heat capacity units
+HEATCAPACITY_DIMENSIONS = [(pq.J / pq.K).simplified.dimensionality]
+MOLAR_HEATCAPACITY_DIMENSIONS = [(pq.J /pq.mol / pq.K).simplified.dimensionality]
+
+def convertHeatCapacity(quantity, units, unittype='heat capacity'):
+    """
+    Convert a given `quantity` -- a :class:`Quantity` object with units of 
+    heat capacity -- to the given `units` of heat capacity. A 
+    :class:`ValueError` is raised if this conversion is not successful. This
+    function can handle conversion between intensive (molar) and extensive heat
+    capacities by multiplying or dividing by an Avogadro number as appropriate.
+    """
+    if isinstance(quantity, tuple):
+        quantity = pq.Quantity(quantity[0], quantity[1])
+    elif not isinstance(quantity, pq.Quantity):
+        raise ValueError('Invalid value "{0}" for quantity; must be a Quantity object with units of {1}.'.format(quantity, unittype))
+
+    if isinstance(units, str):
+        units = pq.Quantity(1.0, units)
+    inputDimensionality = quantity.units.dimensionality.simplified
+    outputDimensionality = units.dimensionality.simplified
+
+    if inputDimensionality not in HEATCAPACITY_DIMENSIONS and inputDimensionality not in MOLAR_HEATCAPACITY_DIMENSIONS:
+        raise ValueError('Invalid input units "{0}" for {1}.'.format(quantity.units, unittype))
+    if outputDimensionality not in HEATCAPACITY_DIMENSIONS and outputDimensionality not in MOLAR_HEATCAPACITY_DIMENSIONS:
+        raise ValueError('Invalid output units "{0}" for {1}.'.format(units, unittype))
+    
+    if inputDimensionality in HEATCAPACITY_DIMENSIONS and outputDimensionality in MOLAR_HEATCAPACITY_DIMENSIONS:
+        quantity = quantity * constants.Na / pq.mol
+    elif inputDimensionality in MOLAR_HEATCAPACITY_DIMENSIONS and outputDimensionality in HEATCAPACITY_DIMENSIONS:
+        quantity = quantity / (constants.Na / pq.mol)
+    
+    return quantity.rescale(units)
+
+def convertEntropy(quantity, units):
+    """
+    Convert a given `quantity` -- a :class:`Quantity` object with units of 
+    entropy -- to the given `units` of entropy. A :class:`ValueError` is 
+    raised if this conversion is not successful. This function can handle 
+    conversion between intensive (molar) and extensive entropies by
+    multiplying or dividing by an Avogadro number as appropriate.
+    """
+    return convertHeatCapacity(quantity, units, 'entropy')
+
+################################################################################
+
+# Allowed simplified frequency dimensionality
+FREQUENCY_DIMENSIONS = [(1./pq.s.simplified).dimensionality, pq.Hz.simplified.dimensionality]
+EXTRA_FREQUENCY_DIMENSIONS = {
+    pq.J.dimensionality: 1.0 / (constants.h * pq.J * pq.s),
+    pq.wavenumber.dimensionality: constants.c * 100. / pq.s / pq.wavenumber,
+    pq.eV.dimensionality: constants.e / (constants.h * pq.eV * pq.s),
+    (1.0/pq.cm).dimensionality: constants.c * 100. / pq.s * pq.cm,
+}
+
+def convertFrequency(quantity, units):
+    """
+    Convert a given `quantity` with units of frequency to the given `units` 
+    of frequency. A :class:`ValueError` is raised if this conversion is not
+    successful.
+    """
+    if isinstance(quantity, tuple):
+        quantity = pq.Quantity(quantity[0], quantity[1])
+    elif not isinstance(quantity, pq.Quantity):
+        raise ValueError('Invalid value "{0}" for quantity; must be a Quantity object with units of frequency.'.format(quantity))
+    
+    if isinstance(units, str):
+        units = pq.Quantity(1.0, units)
+    
+    inputDimensionality = quantity.units.dimensionality
+    outputDimensionality = units.dimensionality
+    
+    if inputDimensionality.simplified not in FREQUENCY_DIMENSIONS and inputDimensionality not in EXTRA_FREQUENCY_DIMENSIONS:
+        raise ValueError('Invalid input units "{0}" for frequency.'.format(quantity.units))
+    if outputDimensionality.simplified not in FREQUENCY_DIMENSIONS and outputDimensionality not in EXTRA_FREQUENCY_DIMENSIONS:
+        raise ValueError('Invalid output units "{0}" for frequency.'.format(units))
+    
+    if inputDimensionality in EXTRA_FREQUENCY_DIMENSIONS:
+        quantity = quantity * EXTRA_FREQUENCY_DIMENSIONS[inputDimensionality]
+    else:
+        quantity = quantity.rescale(pq.Hz)
+    
+    if outputDimensionality in EXTRA_FREQUENCY_DIMENSIONS:
+        return quantity / EXTRA_FREQUENCY_DIMENSIONS[outputDimensionality]
+    else:
+        return quantity.rescale(units)
+
+################################################################################
+
+# Allowed mass units
+MASS_DIMENSIONS = [pq.kg.simplified.dimensionality]
+MOLAR_MASS_DIMENSIONS = [(pq.kg / pq.mol).simplified.dimensionality]
+
+def convertMass(quantity, units):
+    """
+    Convert a given `quantity` -- a :class:`Quantity` object with units of 
+    mass -- to the given `units` of mass. A :class:`ValueError` is raised
+    if this conversion is not successful. This function can handle conversion
+    between intensive (molar) and extensive masses by multiplying or dividing
+    by an Avogadro number as appropriate.
+    """
+    if isinstance(quantity, tuple):
+        quantity = pq.Quantity(quantity[0], quantity[1])
+    elif not isinstance(quantity, pq.Quantity):
+        raise ValueError('Invalid value "{0}" for quantity; must be a Quantity object with units of mass.'.format(quantity))
+    
+    if isinstance(units, str):
+        units = pq.Quantity(1.0, units)
+        
+    inputDimensionality = quantity.units.dimensionality.simplified
+    outputDimensionality = units.dimensionality.simplified
+    
+    if inputDimensionality not in MASS_DIMENSIONS and inputDimensionality not in MOLAR_MASS_DIMENSIONS:
+        raise ValueError('Invalid input units "{0}" for {1}.'.format(quantity.units, unittype))
+    if outputDimensionality not in MASS_DIMENSIONS and outputDimensionality not in MOLAR_MASS_DIMENSIONS:
+        raise ValueError('Invalid output units "{0}" for {1}.'.format(units, unittype))
+    
+    if inputDimensionality in MASS_DIMENSIONS and outputDimensionality in MOLAR_MASS_DIMENSIONS:
+        quantity = quantity * constants.Na / pq.mol
+    elif inputDimensionality in MOLAR_MASS_DIMENSIONS and outputDimensionality in MASS_DIMENSIONS:
+        quantity = quantity / (constants.Na / pq.mol)
+    
+    return quantity.rescale(units)
+
+################################################################################
+
+INERTIA_DIMENSIONS = [(pq.kg * pq.m * pq.m).simplified.dimensionality]
+
+def convertInertia(quantity, units):
+    """
+    Convert a given `quantity` with units of moment of inertia to the given
+    `units` of moment of inertia. A :class:`ValueError` is raised if this 
+    conversion is not successful.
+    """
+    if isinstance(quantity, tuple):
+        quantity = pq.Quantity(quantity[0], quantity[1])
+    elif not isinstance(quantity, pq.Quantity):
+        raise ValueError('Invalid value "{0}" for quantity; must be a Quantity object with units of moment of inertia.'.format(quantity))
+    
+    if isinstance(units, str):
+        units = pq.Quantity(1.0, units)
+    
+    inputDimensionality = quantity.units.dimensionality
+    outputDimensionality = units.dimensionality
+    
+    if inputDimensionality.simplified not in INERTIA_DIMENSIONS:
+        raise ValueError('Invalid input units "{0}" for moment of inertia.'.format(quantity.units))
+    if outputDimensionality.simplified not in INERTIA_DIMENSIONS:
+        raise ValueError('Invalid output units "{0}" for moment of inertia.'.format(units))
+    
+    return quantity.rescale(units)
+
+################################################################################
+
+# Note: There are many possible units for rate coefficients, but they should
+# all produce a combination of m^3, mol, and s when simplified
+RATE_COEFFICIENT_DIMENSIONS = [
+    (1.0/pq.s).simplified.dimensionality, 
+    (pq.m**3/(pq.mol*pq.s)).simplified.dimensionality, 
+    (pq.m**6/(pq.mol**2*pq.s)).simplified.dimensionality, 
+    (pq.m**9/(pq.mol**3*pq.s)).simplified.dimensionality,
+]
+
+def convertRateCoefficient(quantity, units):
+    """
+    Convert a given `quantity` -- a :class:`Quantity` object with units of 
+    rate coefficient -- to the given `units` of rate coefficient. A 
+    :class:`ValueError` is raised if this conversion is not successful.
+    Conversion between molar and molecular bases are possible, as long as you
+    explicitly specify the units of molecules.
+    """
+    if isinstance(quantity, tuple):
+        quantity = pq.Quantity(quantity[0], quantity[1])
+    elif not isinstance(quantity, pq.Quantity):
+        raise ValueError('Invalid value "{0}" for quantity; must be a Quantity object with units of mass.'.format(quantity))
+    
+    if isinstance(units, str):
+        units = pq.Quantity(1.0, units)
+    inputDimensionality = quantity.units.dimensionality.simplified
+    outputDimensionality = units.dimensionality.simplified
+    
+    if inputDimensionality not in RATE_COEFFICIENT_DIMENSIONS:
+        raise ValueError('Invalid input units "{0}" for {1}.'.format(quantity.units, unittype))
+    if outputDimensionality not in RATE_COEFFICIENT_DIMENSIONS:
+        raise ValueError('Invalid output units "{0}" for {1}.'.format(units, unittype))
+    
+    return quantity.rescale(units)
