ceb277e95caf6b4f8becd327952eabc74c4d1192
Josh Allen <jwallen@mit.edu>
2012-02-03 10:34:52 -0500

    Added implementation of RPMD ring polymer time evolution.
    The evolve() subroutine is used to take a single (optionally
    constrained) time step. The position, momentum, potential, force, and
    reaction coordinate are updated by integrating the classical Hamiltonian
    for the ring polymer system. It turns out that the ring polymer term in
    the Hamiltonian is best handled in normal mode space (see
    doi:10.1063/1.3489925 for more); this is implemented in the
    evolve_free_ring_polymer() subroutine.
    
    The Hamiltonian also requires the potential of the system. The
    get_potential() function is set up to call back to Python to evaluate a
    user-provided function for this purpose. This is done purposefully, to
    enable the user to use a quantum chemistry package to get energies if a
    mapped PES is not available.

--- pyrate/rpmd/_main.f90
+++ pyrate/rpmd/_main.f90
@@ -26,6 +26,206 @@
 !
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! 
 
+! Evolve the RPMD system for one time step, optionally applying a constraint
+! that the system remain on the transition state dividing surface.
+! Parameters:
+!   p - The momentum of each bead in each atom
+!   q - The position of each bead in each atom
+!   V - The potential of each bead
+!   dVdq - The force exerted on each bead in each atom
+!   xi - The value of the reaction coordinate
+!   dxi - The gradient of the reaction coordinate
+!   d2xi - The Hessian of the reaction coordinate
+!   Natoms - The number of atoms in the molecular system
+!   Nbeads - The number of beads to use per atom
+!   constrain - 1 to constrain the system to the dividing surface, 0 otherwise
+!   beta - The inverse temperature at which to compute the transmission coefficient
+!   dt - The time step to use in the simulation
+!   mass - The mass of each atom in the molecular system
+!   potential - A function that evaluates the potential and force for a given position
+!   Rinf - The distance at which the reactant interaction becomes negligible
+!   massfrac - The mass fraction of each atom
+!   reactant1_atoms - An array of indices for each atom in the first reactant
+!   Nreactant1_atoms - The number of atoms in the first reactant
+!   reactant2_atoms - An array of indices for each atom in the second reactant
+!   Nreactant2_atoms - The number of atoms in the second reactant
+!   Nts - The number of equivalent transition states that define the dividing surface
+!   forming_bonds - An array listing the pairs of indices of each forming bond in each transition state
+!   forming_bond_lengths - An array listing the lengths of each forming bond in each transition state
+!   number_of_forming_bonds - The number of bonds being formed by the reaction
+!   breaking_bonds - An array listing the pairs of indices of each breaking bond in each transition state
+!   breaking_bond_lengths - An array listing the lengths of each breaking bond in each transition state
+!   number_of_breaking_bonds - The number of bonds being broken by the reaction
+!   xi_current - The maximum of the reaction coordinate at the current temperature
+!   mode - 1 for umbrella integration, 2 for recrossing factor
+! Returns:
+!   p - The updated momentum of each bead in each atom
+!   q - The updated position of each bead in each atom
+!   V - The updated potential of each bead
+!   dVdq - The updated force exerted on each bead in each atom
+!   xi - The updated value of the reaction coordinate
+!   dxi - The updated gradient of the reaction coordinate
+!   d2xi - The updated Hessian of the reaction coordinate
+subroutine evolve(p, q, V, dVdq, xi, dxi, d2xi, Natoms, Nbeads, constrain, &
+  beta, dt, mass, potential, &
+  Rinf, massfrac, reactant1_atoms, Nreactant1_atoms, reactant2_atoms, Nreactant2_atoms, &
+  Nts, forming_bonds, forming_bond_lengths, number_of_forming_bonds, &
+  breaking_bonds, breaking_bond_lengths, number_of_breaking_bonds, &
+  xi_current, mode)
+
+    implicit none
+    external potential
+    integer, intent(in) :: Natoms, Nbeads
+    double precision, intent(inout) :: p(3,Natoms,Nbeads), q(3,Natoms,Nbeads)
+    double precision, intent(inout) :: V(Nbeads), dVdq(3,Natoms,Nbeads)
+    double precision, intent(inout) :: xi, dxi(3,Natoms), d2xi(3,Natoms,3,Natoms)
+    integer, intent(in) :: constrain
+    double precision, intent(in) :: beta, dt, xi_current, mass(Natoms)
+    integer, intent(in) :: mode
+    ! Reactant dividing surface
+    integer, intent(in) :: Nreactant1_atoms, Nreactant2_atoms
+    double precision, intent(in) :: massfrac(Natoms)
+    integer, intent(in) :: reactant1_atoms(Nreactant1_atoms), reactant2_atoms(Nreactant2_atoms)
+    double precision, intent(in) :: Rinf
+    ! Transition state dividing surface
+    integer, intent(in) :: Nts
+    integer, intent(in) :: number_of_forming_bonds, number_of_breaking_bonds
+    integer, intent(in) :: forming_bonds(Nts,number_of_forming_bonds,2)
+    integer, intent(in) :: breaking_bonds(Nts,number_of_breaking_bonds,2)
+    double precision, intent(in) :: forming_bond_lengths(Nts,number_of_forming_bonds)
+    double precision, intent(in) :: breaking_bond_lengths(Nts,number_of_breaking_bonds)
+
+    double precision :: centroid(3,Natoms)
+    integer :: i, j
+
+    ! Update momentum (half time step)
+    p = p - 0.5d0 * dt * dVdq
+
+    ! Update position (full time step)
+    if (Nbeads .eq. 1) then
+        do i = 1, 3
+            do j = 1, Natoms
+                q(i,j,1) = q(i,j,1) + p(i,j,1) * dt / mass(j)
+            end do
+        end do
+    else
+        call evolve_free_ring_polymer(p, q, beta, dt, mass, Natoms, Nbeads)
+    end if
+
+    ! If constrain is on, the evolution will be constrained to the
+    ! transition state dividing surface
+    if (constrain .eq. 1) call constrain_to_dividing_surface(p, q, dxi, mass, &
+        dt, Natoms, Nbeads, &
+        Rinf, massfrac, reactant1_atoms, Nreactant1_atoms, reactant2_atoms, Nreactant2_atoms, &
+        Nts, forming_bonds, forming_bond_lengths, number_of_forming_bonds, &
+        breaking_bonds, breaking_bond_lengths, number_of_breaking_bonds, &
+        xi_current, mode)
+
+    ! Update reaction coordinate and its gradient and Hessian
+    call get_centroid(q, Natoms, Nbeads, centroid)
+    call get_reaction_coordinate(centroid, xi, dxi, d2xi, Natoms, &
+        Rinf, massfrac, reactant1_atoms, Nreactant1_atoms, reactant2_atoms, Nreactant2_atoms, &
+        Nts, forming_bonds, forming_bond_lengths, number_of_forming_bonds, &
+        breaking_bonds, breaking_bond_lengths, number_of_breaking_bonds, &
+        xi_current, mode)
+
+    ! Update potential using new position
+    call get_potential(q, xi, dxi, d2xi, V, dVdq, Natoms, Nbeads, potential)
+
+    ! Update momentum (half time step) using new potential
+    p = p - 0.5d0 * dt * dVdq
+
+    ! Constrain momentum again
+    if (constrain .eq. 1) call constrain_momentum_to_dividing_surface(p, mass, dxi, Natoms, Nbeads)
+
+end subroutine evolve
+
+! Evolve the position and momentum in time according to the free ring polymer
+! term in the Hamiltonian. This is most efficiently done in normal mode space
+! instead of Cartesian space; fast Fourier transforms are employed to move to
+! and from normal mode space.
+! Parameters:
+!   p - The momentum of each bead in each atom
+!   q - The position of each bead in each atom
+!   beta - The inverse temperature at which to compute the transmission coefficient
+!   dt - The time step to use in the simulation
+!   mass - The mass of each atom in the molecular system
+!   Natoms - The number of atoms in the molecular system
+!   Nbeads - The number of beads to use per atom
+! Returns:
+!   p - The updated momentum of each bead in each atom
+!   q - The updated position of each bead in each atom
+subroutine evolve_free_ring_polymer(p, q, beta, dt, mass, Natoms, Nbeads)
+
+    implicit none
+    integer, intent(in) :: Natoms, Nbeads
+    double precision, intent(inout) :: p(3,Natoms,Nbeads), q(3,Natoms,Nbeads)
+    double precision, intent(in) :: beta, dt, mass(Natoms)
+
+    double precision :: poly(4,Nbeads)
+    double precision :: pi, beta_n, twown, pi_n, wk, wt, wm, cos_wt, sin_wt, p_new
+    integer :: i, j, k
+
+    pi = dacos(-1.0d0)
+
+    ! Transform to normal mode space
+    do i = 1, 3
+        do j = 1, Natoms
+            call rfft(p(i,j,:), Nbeads)
+            call rfft(q(i,j,:), Nbeads)
+        end do
+    end do
+
+    do j = 1, Natoms
+
+        poly(1,1) = 1.0d0
+        poly(2,1) = 0.0d0
+        poly(3,1) = dt / mass(j)
+        poly(4,1) = 1.0d0
+
+        if (Nbeads .gt. 1) then
+            beta_n = beta / Nbeads
+            twown = 2.0d0 / beta_n
+            pi_n = pi / Nbeads
+            do k = 1, Nbeads / 2
+                wk = twown * dsin(k * pi_n)
+                wt = wk * dt
+                wm = wk * mass(j)
+                cos_wt = dcos(wt)
+                sin_wt = dsin(wt)
+                poly(1,k+1) = cos_wt
+                poly(2,k+1) = -wm*sin_wt
+                poly(3,k+1) = sin_wt/wm
+                poly(4,k+1) = cos_wt
+            end do
+            do k = 1, (Nbeads - 1) / 2
+                poly(1,Nbeads-k+1) = poly(1,k+1)
+                poly(2,Nbeads-k+1) = poly(2,k+1)
+                poly(3,Nbeads-k+1) = poly(3,k+1)
+                poly(4,Nbeads-k+1) = poly(4,k+1)
+            end do
+        end if
+
+        do k = 1, Nbeads
+            do i = 1, 3
+                p_new = p(i,j,k) * poly(1,k) + q(i,j,k) * poly(2,k)
+                q(i,j,k) = p(i,j,k) * poly(3,k) + q(i,j,k) * poly(4,k)
+                p(i,j,k) = p_new
+            end do
+        end do
+
+    end do
+
+    ! Transform back to Cartesian space
+    do i = 1, 3
+        do j = 1, Natoms
+            call irfft(p(i,j,:), Nbeads)
+            call irfft(q(i,j,:), Nbeads)
+        end do
+    end do
+
+end subroutine evolve_free_ring_polymer
+
 ! Constrain the position and the momentum to the dividing surface, using the
 ! SHAKE/RATTLE algorithm.
 ! Parameters:
@@ -304,6 +504,31 @@ subroutine get_reaction_coordinate(centroid, xi, dxi, d2xi, Natoms, &
     end if
 
 end subroutine get_reaction_coordinate
+
+! Compute the potential for the given position.
+! Parameters:
+!   q - The momentum of each bead in each atom
+!   xi - The value of the reaction coordinate
+!   dxi - The gradient of the reaction coordinate
+!   d2xi - The Hessian of the reaction coordinate
+!   Natoms - The number of atoms in the molecular system
+!   Nbeads - The number of beads to use per atom
+!   potential - A function that evaluates the potential and force for a given position
+! Returns:
+!   V - The potential of each bead
+!   dVdq - The force exerted on each bead in each atom
+subroutine get_potential(q, xi, dxi, d2xi, V, dVdq, Natoms, Nbeads, potential)
+
+    implicit none
+    external potential
+    integer, intent(in) :: Natoms, Nbeads
+    double precision, intent(in) :: q(3,Natoms,Nbeads)
+    double precision, intent(in) :: xi, dxi(3,Natoms), d2xi(3,Natoms,3,Natoms)
+    double precision, intent(out) :: V(Nbeads), dVdq(3,Natoms,Nbeads)
+
+    call potential(q, V, dVdq, Natoms, Nbeads)
+
+end subroutine get_potential
 
 ! Compute the total energy of all ring polymers in the RPMD system.
 ! Parameters:
