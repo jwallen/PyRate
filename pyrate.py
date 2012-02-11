ccd571047f1a84f38ae6ac47d46592d27dc086e1
Josh Allen <jwallen@mit.edu>
2012-02-11 11:57:44 -0500

    Added logging functionality to main pyrate.py executable script.
    This functionality initializes the logging system to write simultaneously
    to both the console (with stderr redirected to stdout) and to a file on
    disk. The log file always has at least verbose detail. After
    initialization, a log header is printed containing information about the
    program. Once the job is finished, a log footer is written to signal that
    the job terminated.

--- pyrate.py
+++ pyrate.py
@@ -49,7 +49,10 @@ for more information.
 
 import os
 import os.path
+import sys
+import logging
 import argparse
+import time
 
 ################################################################################
 
@@ -85,6 +88,96 @@ def parseCommandLineArguments():
 
 ################################################################################
 
+def initializeLog(verbose, logFile):
+    """
+    Set up a logger for PyRate to use to print output to stdout. The
+    `verbose` parameter is an integer specifying the amount of log text seen
+    at the console; the levels correspond to those of the :data:`logging` module.
+    """
+    # Create logger
+    logger = logging.getLogger()
+    logger.setLevel(verbose)
+
+    # Create console handler; send everything to stdout rather than stderr
+    ch = logging.StreamHandler(sys.stdout)
+    ch.setLevel(verbose)
+
+    # Create file handler; always be at least verbose in the file
+    fh = logging.FileHandler(filename=logFile)
+    fh.setLevel(min(logging.DEBUG,verbose))
+
+    # Use custom level names for cleaner log output
+    logging.addLevelName(logging.CRITICAL, 'Critical: ')
+    logging.addLevelName(logging.ERROR, 'Error: ')
+    logging.addLevelName(logging.WARNING, 'Warning: ')
+    logging.addLevelName(logging.INFO, '')
+    logging.addLevelName(logging.DEBUG, '')
+    logging.addLevelName(0, '')
+
+    # Create formatter and add to handlers
+    formatter = logging.Formatter('%(levelname)s%(message)s')
+    ch.setFormatter(formatter)
+    fh.setFormatter(formatter)
+    
+    # Remove old handlers before adding ours
+    while logger.handlers:
+        logger.removeHandler(logger.handlers[0])
+
+    # Add handlers to logger
+    logger.addHandler(ch)
+    logger.addHandler(fh)
+
+def getGitCommit():
+    """
+    Return information about the git commit being used to run the job, if
+    available. An empty string is returned if this information cannot be
+    determined.
+    """
+    try:
+        f = os.popen('git log --format="The current git HEAD is %h (%cd)" -1')
+        msg = f.read()
+        f.close()
+        return msg
+    except IndexError:
+        return ''
+
+def logHeader(level=logging.INFO):
+    """
+    Output a header containing identifying information about PyRate to the log.
+    """
+    logging.log(level, 'PyRate execution initiated at {0}'.format(time.asctime()))
+    logging.log(level, '')
+
+    logging.log(level, '###############################################################')
+    logging.log(level, '#                                                             #')
+    logging.log(level, '#                           ----->                            #')
+    logging.log(level, '#                           PyRate                            #')
+    logging.log(level, '#                           <-----                            #')
+    logging.log(level, '#                                                             #')
+    logging.log(level, '# Version: 0.1.0 (1 Jan 2012)                                 #')
+    logging.log(level, '#                                                             #')
+    logging.log(level, '# Authors: Joshua W. Allen (jwallen@mit.edu)                  #')
+    logging.log(level, '#          Yury V. Suleimanov (ysuleyma@mit.edu)              #')
+    logging.log(level, '#          William H. Green (whgreen@mit.edu)                 #')
+    logging.log(level, '#                                                             #')
+    logging.log(level, '# Website: http://github.com/GreenGroup/PyRate                #')
+    logging.log(level, '#                                                             #')
+    logging.log(level, '###############################################################')
+    logging.log(level, '')
+    
+    gitInfo = getGitCommit()
+    if gitInfo != '':
+        logging.log(level, gitInfo)
+
+def logFooter(level=logging.INFO):
+    """
+    Output a footer to the log.
+    """
+    logging.log(level, '')
+    logging.log(level, 'PyRate execution terminated at {0}'.format(time.asctime()))
+
+################################################################################
+
 if __name__ == '__main__':
     
     # Parse and validate the command-line arguments
@@ -97,3 +190,13 @@ if __name__ == '__main__':
         outputDirectory = os.path.abspath(args.output_directory[0])
     else:
         outputDirectory = os.path.dirname(os.path.abspath(args.file[0]))
+    
+    # Initialize the logging system (both to the console and to a file in the
+    # output directory)
+    initializeLog(args.verbose, os.path.join(outputDirectory, 'pyrate.log'))
+
+    # Print some information to the beginning of the log
+    logHeader()
+    
+    # Print some information to the end of the log
+    logFooter()
